2f82e79e8898a10d8c26290a12efa20e0ef1d1db
19270400 <19270400@qq.com>
2018-05-04 18:05:04 +0800

    new

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,26 @@
+node {
+
+	 env.PATH="${tool 'M3'}/bin:${env.PATH}"
+      
+     stage('checkout') {
+         checkout scm
+     }
+     
+     stage ('clean') {
+         sh "mvn clearn"
+     }
+
+     stage('packaging  sit'){
+         sh "mvn clean package -DskipTests -Psit"
+         step([$class:'ArtifactArchiver',artifacts:'**/target/**.jar'])
+	 }
+	 
+	 stage('deplay sit'){
+	     try {
+		      sh ''anible-playbook sit-node1.yml'
+		 }catch(err){
+		     throw err 
+		 }
+	 }
+}	 
+	 
